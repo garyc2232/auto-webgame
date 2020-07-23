beb8c060e495e81cdc9c9ff7212c3a5124bdf1e7
Garyc <garyc2232@gmail.com>
2020-07-23 13:00:16 +0800

    added forest action

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
 } = require("./config.json");
 const DEBUG_MODE = false;
 const browserSize = {
-    width: 1920 / 2,
+    width: 1920,
     height: 1000
 };
 const args = [];
@@ -19,9 +19,15 @@ const CHALLENGE = "form[action='./siai.cgi'] > input[type=submit]";
 const BATTLE = "div.count-champ.count > input[type=submit]";
 const REFRESH = "form[action='./ffadventure.cgi'] > input[type=submit]";
 const SUPER = "form[action='./monster2.cgi']";
+const FOREST = "form[action='./monster.cgi']";
 const BANK = "form[action='./bank.cgi'] > input[type=submit]";
 const HEAL = "form[action='./yado.cgi'] > input[type=submit]";
-
+const isVisible = async (el, page) => {
+    return await page.evaluate((el) => {
+        const e = document.querySelector(el);
+        return e.style.display === 'inline-block' || e.style.display === 'inline'
+    }, el);
+}
 const savingMoney = async (page) => {
     await page.waitForSelector(BANK, {
         visible: true,
@@ -32,7 +38,7 @@ const savingMoney = async (page) => {
     });
     const currentGold = await page.evaluate(async () => {
         const b = document.querySelectorAll("font > b")[0];
-        return Math.floor(b.innerHTML / 100) - 3
+        return Math.floor(b.innerHTML / 100) - 4
     })
     if (currentGold > 1) {
         const saving = "input[name=azukeru]";
@@ -45,7 +51,8 @@ const savingMoney = async (page) => {
         await page.click(btnSaving);
     }
     DEBUG_MODE && console.log('saving done');
-    await refresh(page);
+    await refresh(page, "saving back to main");
+    await sleep(1000);
 }
 const fightSuper = async (page) => {
     DEBUG_MODE && console.log('super chk');
@@ -57,9 +64,6 @@ const fightSuper = async (page) => {
         });
         await page.click(`${SUPER} > div > input[type=submit]`);
         await backToMain(page);
-        await sleep(500);
-        await page.click(HEAL);
-        await refresh(page);
         DEBUG_MODE && console.log('super end');
         return false;
     } else {
@@ -74,11 +78,11 @@ const backToMain = async (page) => {
     });
     await page.click(btnBack)
 };
-const refresh = async (page) => {
+const refresh = async (page, txt = 'refresh') => {
     await page.waitForSelector(REFRESH, {
         visible: true,
     });
-    DEBUG_MODE && console.log('refresh');
+    DEBUG_MODE && console.log(txt);
     await page.click(REFRESH);
 }
 const baseFight = async (page) => {
@@ -105,7 +109,8 @@ const challage = async (page) => {
         await sleep(250);
         await page.click(CHALLENGE);
         await sleep(1000);
-        await page.select('select[name=sentou]', 'c3511140');
+        const optionID = await page.evaluate(() => document.querySelector('select[name=sentou] option:nth-child(2)').value)
+        await page.select('select[name=sentou]', optionID);
         await backToMain(page);
         await sleep(500);
         await backToMain(page);
@@ -141,6 +146,63 @@ const login = async (user, page) => {
     await sleep(1000);
 }
 
+const forest = async (page) => {
+    DEBUG_MODE && console.log('forest chk');
+
+    if (!await isVisible(`div.count-mori.count > span.msg`, page)) {
+        await savingMoney(page);
+        DEBUG_MODE && console.log('forest start');
+        await page.waitForSelector(`div.count-mori.count > input[type=submit]`, {
+            visible: true,
+        });
+        await page.click(`div.count-mori.count > input[type=submit]`);
+        while (true) {
+            DEBUG_MODE && console.log("forest go");
+            await sleep(1000);
+            if (await page.$(FOREST) !== null) {
+                await page.waitForSelector(`${FOREST} > input[type=submit]`, {
+                    visible: true,
+                });
+                await page.click(`${FOREST} > input[type=submit]`);
+            } else {
+                DEBUG_MODE && console.log('end');
+                await sleep(2000);
+                await page.click(`input[type=submit]`);
+                break;
+            }
+        }
+
+        DEBUG_MODE && console.log('forest end');
+        return false;
+    } else {
+        DEBUG_MODE && console.log('forest pass');
+        return true;
+    }
+}
+
+const upgrade = async (user, page) => {
+    const upgradePage = "form[action='./kunren.cgi'] > input[type=submit]";
+    await login(user, page);
+
+    while (true) {
+        try {
+            console.log('START', user.name);
+            await page.waitForSelector(upgradePage);
+            await page.click(upgradePage);
+            await page.waitForSelector("select[name=kaisuu]");
+            await page.select('select[name=kaisuu]', '10');
+            await sleep(500);
+            await page.click("form[action='./kunren.cgi'] > p > input[type=submit]");
+            await refresh(page, "upgrade back to main");
+        } catch (error) {
+            await page.goto(url);
+            await sleep(1000);
+            await login(user, page);
+            DEBUG_MODE && console.log(`${user.name}: ${error}`);
+        }
+    }
+
+}
 const task = async (user, page) => {
     let loopCount = 0;
 
@@ -151,7 +213,16 @@ const task = async (user, page) => {
             console.log('START', user.name);
             await refresh(page);
             await sleep(1000);
-            await fightSuper(page) && await challage(page) && await baseFight(page);
+            let allSkiped = await fightSuper(page) &&
+                await forest(page) &&
+                await challage(page) &&
+                await baseFight(page);
+            if (!allSkiped) {
+                await sleep(1000);
+                await page.click(HEAL);
+                await sleep(500);
+                await refresh(page, "heal back to main");
+            }
             await sleep(10000);
             loopCount++;
             console.log('END', loopCount);
@@ -170,6 +241,11 @@ const main = async () => {
         headless: !DEBUG_MODE,
         args
     });
+    // let test = account.shift();
+    // const page = await browser.newPage();
+    // await page.setViewport(browserSize);
+    // await page.goto(url);
+    // await task(test, page);
     account.forEach(async (user) => {
         const page = await browser.newPage();
         await page.setViewport(browserSize);
